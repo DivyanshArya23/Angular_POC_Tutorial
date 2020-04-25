3f6dde206da823e77fab556f8c8b63e1560513f1
Divyansh Arya <divyansh15047arya@gmail.com>
2020-04-26 00:23:53 +0530

    Login App Project Initialized

---
+++ test/api/file.php
@@ -0,0 +1,42 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: apllication/json');
+
+?>
+{
+    "obj":[
+        {
+            "name":"Divyansh",
+            "online":true
+        },
+        {
+            "name":"Divyansh",
+            "online":true
+        },
+        {
+            "name":"Divyansh",
+            "online":true
+        },
+        {
+            "name":"Divyansh",
+            "online":true
+        },
+        {
+            "name":"Divyansh",
+            "online":true
+        },
+        {
+            "name":"Divyansh",
+            "online":true
+        },
+        {
+            "name":"Divyansh",
+            "online":true
+        },
+        {
+            "name":"Divyansh",
+            "online":true
+        }
+        ]
+    }
