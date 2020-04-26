eb009bef502b221d69d7d33e586584ab08cb1b64
Divyansh Arya <divyansh15047arya@gmail.com>
2020-04-26 14:51:41 +0530

    remote server auth_part 5

--- test/api/database.php
+++ test/api/database.php
@@ -2,17 +2,20 @@
 
 session_start();
 
-$user = $_SESSION['user'];
+$user='';
+if(!empty($_SESSION['user'])){
+  $user = $_SESSION['user'];
+}
 
 if($user == 'admin'){
   echo '{
     "message": "This is a secret message only for administrator",
-    success": true
+    "success": true
   }';
 } else {
   echo '{
     "message": "You are not Admin",
-    success": false
+    "success": false
   }';
 }
 
